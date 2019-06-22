23a788217dbdff74309dd1dfd7c9cc46a07c6d34
xpf0321 <18813004399@163.com>
2019-06-22 11:11:06 +0800

    给聊天滑块添加点击事件处理程序

--- spa-demo/spa-chart/js/spa.shell.js
+++ spa-demo/spa-chart/js/spa.shell.js
@@ -20,11 +20,20 @@ spa.shell = (function () {
             <div class="spa-shell-foot"></div>
             <div class="spa-shell-chat"></div>
             <div class="spa-shell-modal"></div>
-            `
+            `,
+            chat_extend_time:250,
+            chat_retract_time:300,
+            chat_extend_height:450,
+            chat_retract_height:15,
+            chat_extend_title:'Click to retract',
+            chat_retracted_title:'Click to extend'
         },
-        stateMap = { $container:null },
+        stateMap = {
+            $container:null,
+            is_chat_retracted: true
+        },
         jqueryMap = {},
-        setJqueryMap, initModule;
+        setJqueryMap, toggleChat, onClickChat, initModule;
     //---------------------- Begin Module scope variables------------------------------
 
     //--------------------------------Begin Utility Methods-------------------------------
@@ -34,21 +43,88 @@ spa.shell = (function () {
     // Begin Dom method /setJqueryMap/
     setJqueryMap = function () {
         var $container = stateMap.$container;
-        jqueryMap = {$container:$container};
+        jqueryMap = {
+            $container:$container,
+            $chat:$container.find('.spa-shell-chat')
+        };
     };
     // End Dom method  /setJqueryMap/
+
+    toggleChat = function (do_extend,callback) {
+        console.log(11);
+        var
+            px_chat_ht = jqueryMap.$chat.height(),
+            is_open = px_chat_ht === configMap.chat_extend_height,
+            is_closed = px_chat_ht === configMap.chat_retract_height,
+            is_sliding = !is_open && !is_closed;
+        // aviod race condition
+        if(is_sliding){ return false; }
+
+        // Bagin extend chat slider
+        if(do_extend){
+            jqueryMap.$chat.animate(
+                {height:configMap.chat_extend_height},
+                configMap.chat_extend_time,
+                function () {
+                    jqueryMap.$chat.attr(
+                        'title',configMap.chat_extend_title
+                    );
+                    stateMap.is_chat_retracted = false;
+
+                    if(callback){callback(jqueryMap.$chat);}
+                });
+            return true;
+        }
+        // End extend chat slider
+
+        // Begin retract chat slider
+        jqueryMap.$chat.animate(
+            {height:configMap.chat_retract_height},
+            configMap.chat_retract_time,
+            function () {
+                jqueryMap.$chat.attr(
+                    'title',configMap.chat_retracted_title
+                );
+                stateMap.is_chat_retracted = true;
+                if(callback){callback(jqueryMap.$chat);}
+            }
+        );
+        return true;
+        // End retract chat slider
+    };
+    // End DOM method /toggleChat/
+
+
+
     //----------------------------------End Dom Methods----------------------------------
 
     //--------------------------------Begin Event Methods-------------------------------
+    onClickChat = function (event) {
+        toggleChat(stateMap.is_chat_retracted);
+        return false;
+    };
+
     //--------------------------------End Event Methods-------------------------------
 
 
     //--------------------------------Begin Public Methods-------------------------------
     initModule = function ($container) {
+        // load HTML and map jQuery collections
         stateMap.$container = $container;
         $container.html(configMap.main_html);
         setJqueryMap();
-    }
+
+        // initialize chat slider and bind click handler
+        stateMap.is_chat_retracted = true;
+        jqueryMap.$chat
+            .attr('title',configMap.chat_retracted_title)
+            .click(onClickChat);
+
+        // test toggle
+        // setTimeout(function () {toggleChat(true);},3000)
+        // setTimeout(function () {toggleChat(false);},8000)
+    };
+    // End Public method /initModule/
     return {initModule:initModule}
     //--------------------------------End Public Methods-------------------------------
 }());
