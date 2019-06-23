f688c152de575d1077d21db06b40196378172085
xpf0321 <18813004399@163.com>
2019-06-23 13:38:11 +0800

    4.4.2

--- spa-demo/spa-chart4/js/spa.chat.js
+++ spa-demo/spa-chart4/js/spa.chat.js
@@ -10,19 +10,64 @@ spa.chat = (function () {
    var
        configMap = {
             main_html:`
-            <div style="padding: 1em; color: #fff;">
-                Say hello to chat
+            <div class="spa-chat">
+                <div class="spa-chat-head">
+                    <div class="spa-chat-head-toggle"></div>
+                    <div class="spa-chat-head-title">
+                        Chat
+                    </div>
+                </div>
+                <div class="spa-chat-closer"></div>
+                <div class="spa-chat-sizer">
+                    <div class="spa-chat-msgs"></div>
+                    <div class="spa-chat-box">
+                        <input type="text">
+                        <div>send</div>
+                    </div>
+                </div>
             </div>
             `,
-           settable_map:{}
+           settable_map:{
+               slider_open_time:true,
+               slider_close_time:true,
+               slider_opened_em:true,
+               slider_closed_em:true,
+               slider_opened_title:true,
+               slider_closed_title:true,
+               chat_model:true,
+               people_model:true,
+               set_chat_anchor:true
+           },
+           slider_open_time:250,
+           slider_close_time:250,
+           slider_opened_em:16,
+           slider_closed_em:2,
+           slider_opened_title:'点击关闭',
+           slider_closed_title:'点击打开',
+           chat_model:null,
+           people_model:null,
+           set_chat_anchor:null
        },
-       stateMap = { $container:null },
+       stateMap = {
+       $container:null,
+           $append_target:null,
+           position_type:'closed',
+           px_per_em:0,
+           slider_hidden_px:0,
+           slider_closed_px:0,
+           slider_opened_px:0,
+       },
        jqueryMap = {},
-       setJqueryMap,configModule,initModule;
+       setJqueryMap,getEmSize,setPxSizes,setSliderPostion,
+       onClickToggle,configModule,initModule;
     // ----------------------- 结束-模块全局变量 ---------------------
 
     // ------------------------- 开始-工具类方法 ------------------------
-
+    getEmSize = function (elem) {
+        return Number(
+            getComputedStyle(elem,'').fontSize.match(/\d*\.?\d*/)[0]
+        );
+    };
 
     // ------------------------- 结束-工具类方法 ------------------------
 
@@ -32,16 +77,105 @@ spa.chat = (function () {
     setJqueryMap = function () {
         var $container = stateMap.$container;
         jqueryMap = {$container:$container};
+
+        var $append_target = stateMap.$append_target,
+            $slider = $append_target.find('.spa-chat');
+
+        jqueryMap = {
+            $slider : $slider,
+            $head:    $slider.find('.spa-chat-head'),
+            $toggle:  $slider.find('.spa-chat-head-toggle'),
+            $title:   $slider.find('.spa-chat-head-title'),
+            $sizer:   $slider.find('.spa-chat-head-sizer'),
+            $msgs:    $slider.find('.spa-chat-msgs'),
+            $box:     $slider.find('.spa-chat-box'),
+            $input:   $slider.find('.spa-chat-input input[type=text]')
+        }
     };
     // End Dom method /setJqueryMap/
+
+    // Begin DOM method /setPxSizes/
+    setPxSizes = function () {
+        var px_per_em,opened_height_em;
+        px_per_em = getEmSize(jqueryMap.$slider.get(0));
+        opened_height_em = configMap.slider_opened_em;
+
+        stateMap.px_per_em = px_per_em;
+        stateMap.slider_closed_px = configMap.slider_closed_em * px_per_em;
+        stateMap.slider_opened_px = opened_height_em * px_per_em;
+        jqueryMap.$slider.css({
+           height:(opened_height_em -2 )* px_per_em
+        });
+    };
+    // End Dom method /setPxSizes/
+
+    // Begin DOM method /setSliderPosition/
+    setSliderPostion = function (position_type,callback) {
+        var
+            height_px,animate_time,slider_title,toggle_text;
+        //
+        if(stateMap.position_type === position_type){
+            return true;
+        }
+
+        // prepare animate parameters
+        switch (position_type){
+            case 'opened':
+                height_px = stateMap.slider_opened_px;
+                animate_time = configMap.slider_open_time;
+                slider_title = configMap.slider_opened_title;
+                toggle_text = '=';
+                break;
+            case 'hidden':
+                height_px = 0;
+                animate_time = configMap.slider_open_time;
+                slider_title = '';
+                toggle_text = '+';
+                break;
+            case 'closed':
+                height_px = stateMap.slider_closed_px;
+                animate_time = configMap.slider_close_time;
+                slider_title = configMap.slider_closed_title;
+                toggle_text = '+';
+                break;
+            default: return false;
+        }
+        // animate slider position change
+        stateMap.position_type = '';
+        jqueryMap.$slider.animate(
+            {height:height_px},
+            animate_time,
+            function () {
+                jqueryMap.$toggle.prop('title',slider_title);
+                jqueryMap.$toggle.text(toggle_text);
+                stateMap.position_type = position_type;
+                if(callback){
+                    callback(jqueryMap.$slider);
+                }
+            }
+        )
+    }
+    // End Dom method /setSliderPosition/
 
     // ------------------------- 结束-DOM-方法 ------------------------
 
     // --------------------------Begin Event Handlers--------------------
-
+    onClickToggle = function (event) {
+        var set_chat_anchor = configMap.set_chat_anchor;
+        if(stateMap.position_type === 'opened'){
+            set_chat_anchor('closed');
+        } else if(stateMap.position_type === 'closed'){
+            set_chat_anchor('opened');
+        }
+        return false;
+    };
     // --------------------------End Event Handlers--------------------
 
     // ------------------------------开始-公共方法-------------------------------
+
+    // Begin public method /configModule/
+    // Example  : spa.chat.configModule({ slider_open_em : 18 })
+    // Arguments :
     configModule = function (input_map) {
         spa.util.setConfigMap({
             input_map:input_map,
@@ -50,15 +184,30 @@ spa.chat = (function () {
         });
         return true;
     };
+    // Begin public method /setSliderPosition/
+    //
+    // Example: spa.chat.setSliderPosition('closed');
 
-    initModule = function ($container) {
-        $container.html(configMap.main_html);
-        stateMap.$container = $container;
+    // Begin public method /initModule/
+    // Example   : spa.chat.initModule($('#div_id'))
+    // Purpose   : Directs Chat to offer its capability to the user
+    // Arguments :
+    //      * $append_target
+    initModule = function ($append_target) {
+        $append_target.append(configMap.main_html);
+        stateMap.$append_target = $append_target;
         setJqueryMap();
+        setPxSizes();
+
+        // initialize chat slider to default title and state
+        jqueryMap.$toggle.prop('title',configMap.slider_closed_title);
+        jqueryMap.$head.click(onClickToggle);
+        stateMap.position_type = 'closed';
         return true;
-    }
+    };
 
     return {
+        setSliderPostion:setSliderPostion,
         configModule:configModule,
         initModule:initModule
     }
