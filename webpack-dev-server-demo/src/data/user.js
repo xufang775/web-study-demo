2091148b4633ae97dc80d29c6e75468f64f11774
xpf0321 <18813004399@163.com>
2019-07-19 07:44:53 +0800

    webpack-dev-server-demo

---
+++ webpack-dev-server-demo/src/data/user.js
@@ -0,0 +1,6 @@
+module.exports = [
+    {id:1,user:'xufang',age:18,class:'一年级',sex:'女'},
+    {id:2,user:'小明',age:8,class:'一年级',sex:'男'},
+    {id:3,user:'小红',age:9,class:'二年级',sex:'女'},
+    {id:4,user:'小花',age:8,class:'一年级',sex:'女'},
+]
